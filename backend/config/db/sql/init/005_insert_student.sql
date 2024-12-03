bec2de9c3636f5289e45204cf2880946113e7843
thuongmai <thuongmai10292@gmail.com>
2024-12-03 01:36:21 -0500

    Init tables and sample data

---
+++ backend/config/db/sql/init/005_insert_student.sql
@@ -0,0 +1,5 @@
+INSERT INTO sys_student (username, email, password)
+VALUES
+    ('user1', 'user1@example.com', 'password123'),
+    ('user2', 'user2@example.com', 'password456'),
+    ('user3', 'user3@example.com', 'password789');
